414a0284f0c7fcac791c7e296946ccb4d37c61d2
cecile1012 <roehrigcecil@gmail.com>
2020-02-05 15:18:10 +0100

    Initialisation avec les packages

---
+++ javaCode/LogicielGestion/src/logicielgestion/Responsable.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logicielgestion;
+
+/**
+ *
+ * @author cecil
+ */
+public class Responsable extends Employes {
+    
+    //CONSTRUCTEUR 
+    public Responsable(String n, String p, int mat) {
+        super(n, p, mat);
+    }
+    
+    
+}
