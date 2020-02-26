26a16e98d2a9a3e7a05421462a51744e04226ac9
cecile1012 <roehrigcecil@gmail.com>
2020-02-26 15:16:11 +0100

    no message

--- javaCode/LogicielGestion/src/logicielgestion/LogicielGestion.java
+++ javaCode/LogicielGestion/src/logicielgestion/LogicielGestion.java
@@ -15,7 +15,18 @@ public class LogicielGestion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Création des personnages de l'entreprise 
+        Employes r1 = new Responsable("Biffi", "Emma", 1, 150);
+        Employes r2 = new Responsable("Lacoste", "Thomas", 2, 150);
+        Employes c1 = new Commercial("Cazenave", "Sara", 3, 100, 10);
+        Employes c2 = new Commercial("Freecs", "Gon", 4, 100, 10);
+        Employes b1 = new EmployeDeBase("Uchiha", "Sasuke", 5, 56);
+        Employes b2 = new EmployeDeBase("Elric", "Alphonse", 6, 56);
+        Employes b3 = new EmployeDeBase("Ackerman", "Levi", 7, 56);
+        Employes b4 = new EmployeDeBase("Hyuga", "Hinata", 8, 56);
+        Employes b5 = new EmployeDeBase("Organa", "Leia", 9, 56);
+        Employes b6 = new EmployeDeBase("Croft", "Lara", 10, 56);
+        
     }
     
 }
